c6b2eb3a4558fc7bf1245f2b0c90f344168ce8fd
Karan Joshi <karan.j@crestskillserve.com>
2025-08-25 15:02:20 +0530

    Revert to previous commit.

--- src/agents/blueprints.py
+++ src/agents/blueprints.py
@@ -1,18 +1,18 @@
 blueprints = {
     "bill of sale": {
-        "blueprintArn": "arn:aws:bedrock:us-east-1:970547377074:blueprint/befc64c3cd84",
+        "blueprintArn": "arn:aws:bedrock:us-east-1:970547377074:blueprint/4574101c87aa",
         "stage": "LIVE"
     },
     "compliance pack": {
-        "blueprintArn": "arn:aws:bedrock:us-east-1:970547377074:blueprint/d4785ef4d912",
+        "blueprintArn": "arn:aws:bedrock:us-east-1:970547377074:blueprint/8561830aa5ce",
         "stage": "LIVE"
     },
     "mv-1": {
-        "blueprintArn": "arn:aws:bedrock:us-east-1:970547377074:blueprint/ddd037f2a367",
+        "blueprintArn": "arn:aws:bedrock:us-east-1:970547377074:blueprint/82775278543d",
         "stage": "LIVE"
     },
     "store pack": {
-        "blueprintArn": "arn:aws:bedrock:us-east-1:970547377074:blueprint/e6c0be47b1e4",
+        "blueprintArn": "arn:aws:bedrock:us-east-1:970547377074:blueprint/ec664044bf90",
         "stage": "LIVE"
     }
 }
